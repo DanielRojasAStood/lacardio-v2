cbc496fb8704a5d8f3eadfd73ad8fc9e586c5897
Daniel Rojas <danielrojas243@gmail.com>
2024-07-19 02:50:51 -0500

    update

--- assets/js/main-v2.js
+++ assets/js/main-v2.js
@@ -473,10 +473,22 @@ jQuery(document).ready(function ($) {
       {
         breakpoint: 1024,
         settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
           slidesToShow: 1,
-          centerMode: true,
-          centerPadding: '20px',
-          arrows: false,
+          slidesToScroll: 1,
         },
       },
       {
